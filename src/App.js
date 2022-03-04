3554aa97fb089d4a699ddb0a228b38502910b273
M Emmad Sadiq <emmadsadiq55@gmail.com>
2022-03-05 04:53:58 +0500

    initialize code
    -top bar
    -side drawer
    -theme
    -ag grid
    -graph
    -form with react form hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import DashboardWrapper from "./components/dashboard/DashboardWrapper";
+import Customer from "./screen/Customer";
+import Home from "./screen/Home";
+import './style/app.css'
 function App() {
+  const [darkTheme, setDarkTheme] = useState(true);
+  useEffect(() => {
+    document.body.style.backgroundColor = darkTheme ? "rgb(0, 30, 60)" : "#edf3f7"
+  }, [darkTheme])
+
+  const handleTheme= () =>{
+    setDarkTheme(!darkTheme)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className={darkTheme ? "my-dark-theme": "my-light-theme"}>
+        <DashboardWrapper handleTheme={handleTheme}>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/customers" element={<Customer />}></Route>
+          </Routes >
+        </DashboardWrapper>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/datatables/Datatables.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
+
+const Datatables = () => {
+    const [rowData] = useState([
+        { make: "Toyota", model: "Celica", price: 35000, price1: 35000, price2: 35000, price3: 35000, price4: 35000 },
+        { make: "Toyota", model: "Celica", price: 35000, price1: 35000, price2: 35000, price3: 35000, price4: 35000 },
+        { make: "Toyota", model: "Celica", price: 35000, price1: 35000, price2: 35000, price3: 35000, price4: 35000 },
+        { make: "Toyota", model: "Celica", price: 35000, price1: 35000, price2: 35000, price3: 35000, price4: 35000 },
+        { make: "Toyota", model: "Celica", price: 35000, price1: 35000, price2: 35000, price3: 35000, price4: 35000 },
+        { make: "Toyota", model: "Celica", price: 35000, price1: 35000, price2: 35000, price3: 35000, price4: 35000 },
+        { make: "Toyota", model: "Celica", price: 35000, price1: 35000, price2: 35000, price3: 35000, price4: 35000 },
+        { make: "Toyota", model: "Celica", price: 35000, price1: 35000, price2: 35000, price3: 35000, price4: 35000 },
+        { make: "Toyota", model: "Celica", price: 35000, price1: 35000, price2: 35000, price3: 35000, price4: 35000 },
+        { make: "Toyota", model: "Celica", price: 35000, price1: 35000, price2: 35000, price3: 35000, price4: 35000 },
+        { make: "Toyota", model: "Celica", price: 35000, price1: 35000, price2: 35000, price3: 35000, price4: 35000 },
+        { make: "Toyota", model: "Celica", price: 35000, price1: 35000, price2: 35000, price3: 35000, price4: 35000 },
+    ]);
+
+    const [columnDefs] = useState([
+        { field: "make", headerName: 'Make', sortable: true, filter: true, resizable:true },
+        { field: "model", headerName: 'Model', sortable: true, filter: true,resizable:true },
+        { field: "price", headerName: 'Price', sortable: true, filter: true ,resizable:true},
+        { field: "price1", headerName: 'Price 1', sortable: true, filter: true ,resizable:true},
+        { field: "price2", headerName: 'Price 2', sortable: true, filter: true,resizable:true },
+        { field: "price3", headerName: 'Price 3', sortable: true, filter: true,resizable:true },
+        { field: "price4", headerName: 'Price 4', sortable: true, filter: true,resizable:true },
+    ]);
+
+    return (
+        <div className="" style={{ maxHeight: 400 }}>
+            <div className="ag-theme-alpine-dark" >
+                <AgGridReact
+                    rowData={rowData}
+                    columnDefs={columnDefs}
+                    enableFilter={true}
+                >
+
+                </AgGridReact>
+            </div>
+        </div>
+    );
+};
+export default Datatables
